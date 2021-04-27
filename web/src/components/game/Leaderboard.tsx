43453b1bf28828e636547d2495045e6c880468a3
Oluwakorede Fashokun <koredefashokun@gmail.com>
2021-04-27 23:02:06 +0100

    Minor improvements for leaderboard styles

--- web/src/components/game/Leaderboard.tsx
+++ web/src/components/game/Leaderboard.tsx
@@ -1,19 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
 import { GameContext } from '../../contexts/GameContext';
+
+const LeaderboardContainer = styled.div`
+	max-width: 600px;
+	max-height: 800px;
+	height: 100%;
+	width: 100%;
+
+	h2 {
+		font-size: 32px;
+		font-weight: 500;
+		margin-bottom: 16px;
+	}
+
+	div {
+		width: 100%;
+		display: flex;
+		justify-content: space-between;
+	}
+`;
 
 const Leaderboard: React.FC = () => {
 	const { state } = React.useContext(GameContext);
 
 	return (
-		<div>
-			<p>Leaderboard</p>
+		<LeaderboardContainer>
+			<h2>Leaderboard</h2>
 			{state.leaderboard?.map(player => (
-				<div key={player._id}>
+				<div
+					key={player._id}
+					style={{
+						width: '100%',
+						display: 'flex',
+						justifyContent: 'space-between'
+					}}
+				>
 					<p>{player.nickname}</p>
 					<p>{player.score}</p>
 				</div>
 			))}
-		</div>
+		</LeaderboardContainer>
 	);
 };
 
--- web/src/components/game/QuestionOption.tsx
+++ web/src/components/game/QuestionOption.tsx
@@ -17,6 +17,7 @@ const QuestionOptionWrapper = styled.button`
 	font-weight: 500;
 	text-align: center;
 	background-color: #ffffff;
+	cursor: pointer;
 
 	&:hover {
 		background-color: #d6d6d6;
