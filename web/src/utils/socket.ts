383f19dc3dba343dc850af373af6fe342351c39b
Oluwakorede Fashokun <koredefashokun@gmail.com>
2021-04-30 22:50:50 +0100

    Store API URL in env var

--- web/src/utils/socket.ts
+++ web/src/utils/socket.ts
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
 
-export const socket = io('http://localhost:5050', {
+export const socket = io(process.env.REACT_APP_API_URL as string, {
 	transports: ['websocket']
 });
