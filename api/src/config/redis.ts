f39022a8a7d3c86744f246cd97bc346344131eaf
Oluwakorede Fashokun <koredefashokun@gmail.com>
2021-07-29 02:17:13 +0100

    Fix the Redis connection issue?

--- api/src/config/redis.ts
+++ api/src/config/redis.ts
@@ -1,5 +1,21 @@
 import redis from 'redis';
 
-const client = redis.createClient({ url: process.env.REDIS_URL });
+const client = redis.createClient({
+	url: process.env.REDIS_URL,
+
+	...(process.env.NODE_ENV === 'production'
+		? {
+				tls: { rejectUnauthorized: false }
+		  }
+		: {})
+});
+
+client.on('connect', () => {
+	console.log('Connected to Redis!');
+});
+
+client.on('error', (error: Error) => {
+	console.log(error);
+});
 
 export default client;
